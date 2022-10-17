f74c2690d115b95177e875e0bf9b5f7f481dd4aa
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-10-17 11:27:53 -0500

    completed 1st array manipulation exercise

---
+++ js/array-manipulation-lec.js
@@ -0,0 +1,36 @@
+// let nums = [6, 2, 4, 4, 5, 1, 6, 5, 8, 9, 10, 1, 12]
+
+// console.log(nums.push(13));
+// nums.push(11, 12, 13)
+// nums.unshift(-2, -1, 0)
+// console.log(nums)
+
+// nums.pop();
+// nums.shift()
+// nums.shift();
+// console.log(nums);
+
+let nameArr = ["jim", "joe", "jim", "john", "jim", "bob"];
+
+// console.log(nameArr.indexOf("john"));
+// console.log(nameArr.indexOf("jim"));
+// console.log(nameArr.lastIndexOf("jim"));
+
+//first arg will dictate that you remove everything before that index.
+// Second arg dictates that you remove everything past that index and that index itself.
+//use negative numbers to go backwards
+// console.log(nameArr.slice(2, 3));
+// nameArr = nameArr.slice(2, 3);
+
+// console.log(nameArr.reverse());
+// console.log(nameArr[0]);
+
+// nameArr.sort()
+// nums.sort();
+// console.log(nums)
+
+// let nameString = "jim, joe, jim, john, jim, bob";
+// // console.log(nameString.split(",", 2))
+// console.log(nameString.split(", "))
+
+console.log(nameArr.join(", "))
