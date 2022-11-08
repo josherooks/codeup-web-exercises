b7554f2dfd52808ad0e02a00a54e87508e145ef1
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-11-08 10:05:37 -0600

    updated notes

---
+++ js/arrays-lec.js
@@ -0,0 +1,40 @@
+// console.log([1, 2, 3, 4, 5, 6]);
+let names = ["john", "jim", "jim", "jim","will"];
+// function hello(){
+//     return"hello";
+// }
+// let arr = [true, 1, "hello", hello];
+// console.log(arr);
+// console.log(arr.toString());
+// console.log(garr.join());
+// console.log(arr.length);
+// console.log(names.length)
+
+// let str = arr.toString();
+// let str = arr.join();
+// console.log(typeof str);
+// console.log(str);
+
+// console.log(names[0])
+// console.log(names[4])
+// console.log(names.length)
+// console.log(names.lastIndexOf("jim"))
+//
+// let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(newArr[0]);
+
+
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+// let lastIndex = names.length -1;
+// for(let i = lastIndex; i >= 0; i--){
+//     console.log(names[i]);
+// }
+
+names.forEach(function (name){
+    console.log(name);
+    // name = "newName";
+    // console.log(name)
+})
+console.log(names)
