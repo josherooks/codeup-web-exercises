ac16781dc218bf482a0c868ecbb0b22a818609aa
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-11-16 10:34:46 -0600

    completed jQuery secectors exercise

--- js/jQuery_exercises.js
+++ js/jQuery_exercises.js
@@ -1,8 +1,31 @@
 "use strict";
-(function (){
+(function () {
+    $(document).ready(function () {
 
-    $(function() {
-        alert( 'The DOM has finished loading!' );
+        //first exercise
+        // $(function () {
+        //     alert('The DOM has finished loading!');
+        // });
+
+        // jQuery id selectors
+        let headingContents = $('#heading').html();
+        alert(headingContents);
+
+        let paragraphContents = $('#paragraph').html();
+        alert(paragraphContents);
+
+        // jQuery class selectors
+        $('.codeup').css('border', '1px solid red');
+
+        // jQuery element selectors
+        $('li').css('font-size', '20px')
+
+        let h1contents = $('h1').html();
+        alert(h1contents);
+
+        // jQuery multiple selectors
+        $('h1, p, li').css('background-color', 'yellow')
+
+
     });
-
 })();
