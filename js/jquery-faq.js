7405720cbdad925f7c48d328a138bb595545c948
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-11-17 15:02:28 -0600

    started jquery faq exercise

---
+++ js/jquery-faq.js
@@ -0,0 +1,14 @@
+"use strict";
+(function (){
+
+    $(document).ready(function(){
+
+
+        $('dd').addClass('invisible');
+        $('.invisible').css('visibility', 'hidden');
+
+
+
+    });
+
+})();
