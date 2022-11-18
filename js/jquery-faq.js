972a01302c6b6174c2050d503e3bcc943061fe56
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-11-18 09:40:28 -0600

    jquery faq exercise walk through

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -3,16 +3,25 @@
 
     $(document).ready(function(){
 
-
+        //
         $('dd').addClass('invisible').addClass('not-invisible');
         $('.invisible').css('visibility', 'hidden');
         $('.not-invisible').css('visibility', "visible");
 
+        // Travesing Exercise
         $('#highlight-last').click(function(){
-            $('.lists').last().css('background-color', 'yellow' )
-        })
+            $('ul').each(function(){
+                $(this).children().last().css('background-color', 'yellow' )
+            });
+        });
 
+        $('h3').click(function(){
+           $(this).next().css('font-weight', 'bold');
+        });
 
+        $('li').click(function(){
+            $(this).parent().children().first().css('color', 'blue');
+        });
     });
 
 })();
