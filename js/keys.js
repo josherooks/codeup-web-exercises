feaefd94f7c0008ea5ff55f43d602f965813009d
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-11-28 09:39:09 -0600

    started weather map project

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,6 @@
 //this is for secret keys and
 
 const MAPBOX_TOKEN = "pk.eyJ1Ijoicm9va3M0NTA3IiwiYSI6ImNsYXF4eDhxNDBlN2czb29hZ3QydTNwdG4ifQ.m2hJE4NVZ4uQQQCLX1MIZA";
+
+const OPEN_WEATHER_APPID = f389bcdbd3b1279daa68510221a632ee
+;
